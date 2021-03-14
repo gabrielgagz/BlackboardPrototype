ad3058a642a69e0341c8222c1b498c1be1fea2b5
gabrielgagz <g.a.gomez@gmail.com>
2021-03-14 19:53:46 -0300

    Fix Blackboard component test
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- client/src/tests/components/Blackboard.test.js
+++ client/src/tests/components/Blackboard.test.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
 import { Blackboard } from '../../components/Blackboard';
 
 describe('Blackboard component tests', () => {
 
     test('Component should be rendered', () => {
         
-        const wrapper = shallow( <Blackboard /> );
+        const wrapper = shallow( 
+                                <MemoryRouter>
+                                    <Blackboard /> 
+                                </MemoryRouter>    
+                                );
 
         expect( wrapper ).toMatchSnapshot();
 
